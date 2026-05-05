db3423917f77bbc41ad7ba8916ad37c95b0f2be5
rtimmaraju <rtimmaraju@blackberry.com>
2026-05-05 16:00:43 +0530

    sys/time.h included

--- src/librtlsdr.c
+++ src/librtlsdr.c
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#include <sys/time.h>
 #include <errno.h>
 #include <signal.h>
 #include <string.h>
